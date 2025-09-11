d50dd9e42ca621776139899a654d769e9012eba8
jovpbcfd <jovan_pbcfd@panalobet.com>
2025-09-11 10:26:20 +0800

    Add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="max-w-[500px] min-h-screen w-full bg-cover bg-no-repeat bg-[url(/img/background_website.png)]">
+		<div className="max-w-[500px] min-h-screen w-full bg-cover bg-no-repeat bg-[url(/img/background.png)] md:bg-[url(/img/background_website.png)]">
 			<div className="flex items-center justify-center">
 				<Image
 					src="/img/upper_website.png"
 					alt="upper"
 					width={500}
 					height={300}
+					className="hidden md:block"
+				/>
+				<Image
+					src="/img/upper.png"
+					alt="upper"
+					width={500}
+					height={300}
+					className="block md:hidden"
 				/>
 			</div>
 
 			{/* frame */}
-			<div className="flex items-center justify-center mt-4">
+			<div className="flex items-center justify-center -mt-5">
 				<iframe
 					src="https://api.g1388.makethatold.com/promobot/lobby?userType=guest&campaignName=DEMO%20KYC%200502&currency=demo_promobot_voucher_bdt"
 					width="80%"
@@ -23,8 +32,34 @@ export default function Home() {
 				/>
 			</div>
 
-			{/* div */}
-			<div></div>
+			{/* download */}
+			<div className="flex items-center justify-center">
+				<Link href="#">
+					<Image
+						src="/img/download_button.png"
+						width={400}
+						height={130}
+						alt="Download button"
+					/>
+				</Link>
+			</div>
+
+			<div className="flex items-center justify-center">
+				<Image
+					src="/img/lower_website.png"
+					width={400}
+					height={200}
+					alt="Millionaire"
+					className="hidden md:block"
+				/>
+				<Image
+					src="/img/lower.png"
+					width={400}
+					height={200}
+					alt="Millionaire"
+					className="block md:hidden"
+				/>
+			</div>
 		</div>
 	);
 }
