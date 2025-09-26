e97613f94f38d1e9296651e6fbcd7ab3d70c05ea
jovpbcfd <jovan_pbcfd@panalobet.com>
2025-09-26 13:26:44 +0800

    Update pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="max-w-[500px] min-h-screen w-full bg-cover bg-no-repeat bg-[url(/img/background.png)] md:bg-[url(/img/background_website.png)]">
-			<div className="flex items-center justify-center">
+			<div className="flex items-center justify-center relative">
 				<Image
 					src="/img/upper_website.png"
 					alt="upper"
@@ -19,6 +19,26 @@ export default function Home() {
 					height={140}
 					className="block h-[140px] -mt-2 md:hidden"
 				/>
+				<div className="absolute inset-0 bg-black/60"></div>
+				<div className="absolute md:bottom-20">
+					<h1 className="text-center leading-5 md:leading-6">
+						<span className="text-md font-bold text-white uppercase md:text-[#1a9e56] md:text-5xl md:font-extrabold md:drop-shadow-[2px_2px_0px_rgba(255,246,154,1)]">
+							99 Free Spins
+						</span>
+						<br />
+						<span className="text-sm drop-shadow-md font-bold text-white md:italic md:font-extrabold md:uppercase">
+							Chance to win a huge jackpot!
+						</span>
+						<br />
+						<span className="text-sm drop-shadow-md font-bold text-white md:italic md:font-extrabold md:uppercase ">
+							Winnings will be instantly credited to your account.
+						</span>
+						<br />
+						<span className="text-sm drop-shadow-md font-bold text-white md:italic md:font-extrabold md:uppercase">
+							Seize this opportunity and play now!
+						</span>
+					</h1>
+				</div>
 			</div>
 
 			{/* frame */}
@@ -55,14 +75,14 @@ export default function Home() {
 
 			<div className="flex items-center justify-center">
 				<Image
-					src="/img/lower_website.png"
+					src="/img/lower_website_v2.png"
 					width={400}
 					height={200}
 					alt="Millionaire"
 					className="hidden md:block"
 				/>
 				<Image
-					src="/img/lower.png"
+					src="/img/lower_V2.png"
 					width={400}
 					height={200}
 					alt="Millionaire"
